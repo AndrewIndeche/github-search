0b71aaf12472d25f212d989ae0698a0510841ff9
AndrewIndeche <indecheandrew@gmail.com>
2021-08-28 12:37:12 +0300

    Add Nav bar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { TopnavbarComponent } from './topnavbar/topnavbar.component';
+import { BottomnavbarComponent } from './bottomnavbar/bottomnavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    TopnavbarComponent,
+    BottomnavbarComponent
   ],
   imports: [
     BrowserModule,
