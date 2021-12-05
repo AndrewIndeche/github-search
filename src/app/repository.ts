2b77cfd8552dbcc5c96e889e0e9a24c16fdd48f9
AndrewIndeche <indecheandrew@gmail.com>
2021-12-05 22:31:57 +0300

    Auto-generated commit [ci skip]

--- src/app/repository.ts
+++
@@ -1,3 +0,0 @@
-export class Repository {
-   constructor(public name:string,public description,public url:string,public updates:Date){}
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const Environment = {
-  production: true,
-  clientId:"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",
-};
